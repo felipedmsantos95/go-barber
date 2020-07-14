50d6581b21f74f9b002b448b8deb0b0a720f9d72
Felipe Santos <fdms@icomp.ufam.edu.br>
2020-07-14 17:41:14 -0400

    integration with postgres

--- backend/src/database/migrations/1594693805989-CreateAppointments.ts
+++ backend/src/database/migrations/1594693805989-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1594693805989
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
