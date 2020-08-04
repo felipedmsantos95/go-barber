470b66c1ec39c02060a241aa5e4381df1f3c0492
Felipe Santos <fdms@icomp.ufam.edu.br>
2020-08-03 22:41:42 -0400

    appling concepts of DDD in app backend

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,9 @@
 /* eslint-disable camelcase */
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import Appointment from '../models/Appointment';
+import AppError from '@shared/errors/AppError';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import AppointmentsReposiotry from '../repositories/AppointmentsRepository';
-import AppError from '../errors/AppError';
 
 interface Request {
     provider_id: string;
